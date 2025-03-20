d8980d0e67ef2a91134e5216176ccbb15efecad8
stvncode <steven.travers20@gmail.com>
2025-03-20 14:29:13 +0100

    Add query Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import "@/styles/globals.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
 import { createAppRouter } from "./config/router"
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={createAppRouter()} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={createAppRouter()} />
+    </QueryClientProvider>
   </StrictMode>
 )
